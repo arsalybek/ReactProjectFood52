9716ddbcf0392471732ac3cbcfc1fac0bdd89970
Ayana Adilova <aadilova19@gmail.com>
2020-12-18 15:46:17 +0600

    fix auth tsx

--- src/components/auth/Auth.tsx
+++ src/components/auth/Auth.tsx
@@ -1,64 +1,8 @@
-// import React, {  ReactElement } from "react";
-// import { User } from "../../models/User";
-// import Field from "../../shared/Field";
-// import { Link } from 'react-router-dom';
-// import './style.css'
-
-// interface Props {
-//   login: (user: User) => void;
-//   cancel: () => void;
-// }
-// export const AppContext = React.createContext({ 
-//   authenticated: true,
-//   lang: 'en',
-//   theme: 'dark'
-// });
-
-// // type ContextProps = { 
-// //   authenticated: boolean,
-// //   lang: string,
-// //   theme: string
-// // };
-
-// // export const AppContext = 
-// //   React.createContext<Partial<ContextProps>>({});
-// export default function Auth({ login, cancel }: Props): ReactElement {
-//   let user: User = { email: "", id: 0, password: "", name: "" };
-
-//   return (
-    
-//     <div className="fields">
-//       <Field
-//         type="email"
-//         label="Email"
-//         onChange={(e) => {
-//           user.email = e.target.value;
-//         }}
-//         required
-//       />
-//       <Field
-//         type="password"
-//         label="Password"
-//         onChange={(e) => {
-//           user.password = e.target.value;
-//         }}
-//         required
-//       />
-
-//       <div className="buttons">
-//       <Link to="/recipes" className="btn btn-primary btn-lg" onClick={() => login(user)}>Submit</Link>
-//       <button className="btn btn-warning btn-lg" onClick={cancel}>Cancel</button>        
-//       </div>
-
-     
-// </div> 
-//   );
-// }
 import React, {  ReactElement } from "react";
+import { User } from "../../models/User"
 import { Link } from 'react-router-dom';
 import './auth.css'
 import { useForm } from "react-hook-form";
-import { User } from "../../models/User";
 
 interface Props {
   login: (user: User) => void;
@@ -82,22 +26,9 @@ export default function Auth({ login, cancel }: Props): ReactElement {
     console.log("data", data);
   };
 
-
   return (
     <div className="fields">
       <form onSubmit={handleSubmit(onSubmit)}>
-      {/* <div className="field">
-        <label htmlFor="name">Name</label>
-        <input
-          type="text"
-          id="name"
-          name="name"
-          ref={register({ required: true })}
-        />
-        {errors.name && errors.name.type === "required" && (
-          <div className="error">Your must enter your name.</div>
-        )}
-      </div> */}
       <div className="field">
         <label htmlFor="email">Email</label>
         <input
@@ -122,16 +53,20 @@ export default function Auth({ login, cancel }: Props): ReactElement {
           type="text"
           id="score"
           name="password"
-          ref={register({ pattern: /^[A-Za-z]+$/i, required: true})}
-          // ref={register({ required: true })}
+          // ref={register({ pattern: /^[A-Za-z]+$/i, required: true})}
+          ref={register({ required: true })}
         />
         {errors.password && errors.password.type === "required" && (
           <div className="error">Your must enter your score.</div>
         )}
+        
       </div>
-      <button className="btn-primary btn-lg" onClick={() => login(user)} type="submit">Submit</button>
-      <Link to="/recipes"  type="submit" className="a" onClick={() => login(user)}>a</Link>
-      <button className="btn-warning btn-lg" onClick={cancel}>Cancel</button>
+      <div className="buttonsLogin">
+      {/* <button className="btn1" onClick={() => login(user)} type="submit">Submit</button> */}
+      <Link to="/recipes"  type="submit" className="submit" onClick={() => login(user)}>Submit</Link>
+      <button className="btn1" onClick={cancel}>Cancel</button>
+      </div>
+      
       
     </form>
     </div>
