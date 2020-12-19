3c184bb85376a59a78d57a29e7d40aa350691dbd
Ayana Adilova <aadilova19@gmail.com>
2020-12-19 10:54:39 +0600

    css

--- src/components/shop/shop.tsx
+++ src/components/shop/shop.tsx
@@ -12,9 +12,16 @@ interface Props {
 }
 
 export default function Shop({shopList}: Props): ReactElement {
-    
+    // const handleRemove = React.useCallback(
+    //     (id) => setUsers(shopList.filter((u) => u.id !== id)),
+    //     [users]
+    //   );
     return (
         <>
+        
+        
+        <div className="content">
+        <span id="theme">OUR BESTSELLERS</span>
         <div id="main-header__menu" className="main-header__menu">
 			<div className="search">
 				<form className="search__form">
@@ -24,21 +31,28 @@ export default function Shop({shopList}: Props): ReactElement {
 				</form>
 			</div>            
 		</div>
-        
-        <div className="content">
-        <span id="theme">OUR BESTSELLERS</span>
-        <hr></hr>
+        {/* <hr></hr> */}
         </div>
         
          <div className="container2">
-            {shopList.map((category) => {
+            {shopList.map((product) => {
                 return<ul className="catalog">            
-				<li className="recipe-li">
-					<img className="card__image" src={category.image} alt="test"/>
+				<li className="recipe-li" key={product.id}>
+					<img className="card__image" src={product.image} alt="test"/>
 					<div className="card__description">
-                        <h3 className="card__header">{category.name}</h3>
-                        <div className="card__price">{category.price} $</div>
-                        <div className="card__price">Rewiew: {category.review_count}<i className="fa fa-star" aria-hidden="true"></i></div>
+                        <h3 className="card__header">{product.name}</h3>
+                        <div className="card__price">{product.price} $</div>
+                        <div className="card__price">Rewiew: {product.review_count}
+                        <div className="rating">
+                        {/* <span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span> */}
+                        <div className="clip-star"></div>
+                        <div className="clip-star"></div>
+                        <div className="clip-star"></div>
+                        <div className="clip-star"></div>
+                        <div className="clip-star"></div>
+                        </div>                      
+                        
+                        </div>
                         <button>add to cart</button>
 					</div>
 				</li>
